c8c58d661fe6ed6b24bc769b00cf1970b213eff4
Gowardhan0 <141393138+Gowardhan0@users.noreply.github.com>
2023-08-04 12:27:25 +0530

    Create Firstprogram.py

---
+++ Firstprogram.py
@@ -0,0 +1,5 @@
+import pandas as pd
+import io 
+data = pd.read_csv(io.BytesIO(uploaded['Uploads_Business-employment-data_Business-employment-data-March-2023-quarter_Download-data_business-employment-data-march-2023-quarter-regional-council-revisions (1).csv'])) 
+
+print(data)
