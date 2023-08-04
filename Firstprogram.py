d895e5034be3c13b60c167b24a0f7443adb73206
Gowardhan0 <141393138+Gowardhan0@users.noreply.github.com>
2023-08-04 12:30:22 +0530

    Firstprogram.py

--- Firstprogram.py
+++ Firstprogram.py
@@ -3,3 +3,9 @@ import io
 data = pd.read_csv(io.BytesIO(uploaded['Uploads_Business-employment-data_Business-employment-data-March-2023-quarter_Download-data_business-employment-data-march-2023-quarter-regional-council-revisions (1).csv'])) 
 
 print(data)
+
+print("Head")
+data.head()
+
+print("Tail")
+data.tail()
